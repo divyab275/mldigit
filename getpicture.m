6ff5eb1977fed9a5ffac16e780fe25089e56221c
nandukmg <nandukrishnan55555@gmail.com>
2018-01-07 00:22:45 +0530

    this thing is ONgit add .!

---
+++ getpicture.m
@@ -0,0 +1,12 @@
+%captures a pic from webcam and stores it as e.jpg .place our number near the camera before calling this
+
+
+pkg load image-acquisition;
+obj = videoinput("v4l2", "/dev/video0");
+get(obj);
+set(obj, "VideoFormat", "RGB3");
+set(obj, "VideoResolution", [320 240]);
+start(obj, 2);
+img = getsnapshot(obj);
+image(img);
+imwrite(img, "e.jpg");
