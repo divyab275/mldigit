e2a850b8efafe573b016b8cb96da977c2cad6ae9
divyab275 <divyab275@gmail.com>
2018-01-13 04:37:58 +0530

    Adds the sample to the new dataset and writes to file

--- loop.m
+++ loop.m
@@ -1,8 +1,16 @@
-i = 0;
-while (i!=11)
-  getpicture
-  t = imageTo20x20Gray('e.jpg',0,0,Theta1,Theta2);
-  i = predict(Theta1,Theta2,t)
-  pause(0.5);
-  %title(gca,num2str (i));
+
+  i=0;
+  while (i!=11)
+    getpicture
+    t = imageTo20x20Gray('e.jpg',0,0,Theta1,Theta2);
+    i = predict(Theta1,Theta2,t)
+    pause(0.5);
+    t = reshape(t,1,400);
+    k = input("Press 1 to add to the dataset");
+    if(k==1)
+      x = [x;t];
+      Y = [Y;label];
+      save("dataset.mat",'x','Y');
+    end
+    % title(gca,num2str (i));
   endwhile
