1cb8e14f15c08014c6f6c09f4d6d772cfa5df9d4
nandukmg <nandukrishnan55555@gmail.com>
2018-01-17 22:58:26 +0530

    added 9

--- loop.m
+++ loop.m
@@ -1,12 +1,13 @@
-
+p = input("Enter crop ratio:");
   i=0;k=0;
   while (i!=11)
     getpicture
-    t = imageTo20x20Gray('e.jpg',0,0,Theta1,Theta2);
+    t = imageTo20x20Gray('e.jpg',p,0,Theta1,Theta2);
     i = predict(Theta1,Theta2,t)
-    pause(0.5);
+    pause(0.25);
     t = reshape(t,1,400);
-    k = input("Press 1 to add to the dataset");
+    %k = input("Press 1 to add to the dataset");
+    
     if(k==1)
       X = [X;t];
       y = [y;label];
